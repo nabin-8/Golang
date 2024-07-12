4a4a6538014b4f6a7b923bb9bfb878096f4c959e
nabin-8 <nabinacharya740@gmail.com>
2024-07-12 19:57:32 +0545

    simple datatypes

---
+++ 2_simple_value/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Simple values
+	// int
+	fmt.Println(1 + 1)
+	// string
+	fmt.Println("hello Golang")
+	// bool
+	fmt.Println(false)
+	fmt.Println(true)
+	// float
+	fmt.Println(10.5)
+	fmt.Println(7.0 / 3.0)
+}
