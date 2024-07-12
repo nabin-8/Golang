9949a4f169e3fad96659a3a9c03666b2a7e2320a
nabin-8 <nabinacharya740@gmail.com>
2024-07-12 19:57:45 +0545

    declare variable

---
+++ 3_variables/variables.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "Golang"
+
+	// automatically type infer
+	// var name = "Golang"
+	// var isAdult=true
+
+	// var age int = 22
+	// there is different int16, 32, 64
+	// if you use int only golang automatically manage according to its assign value internally
+
+	// short hand syntax
+	// name := "Golang"
+	// in golang := is used when we declare value and same time assigned into it
+
+	// then why we use upper two ways to declare variables
+	// var name string
+	// if we have declare variable but not assigned value
+	// name = "Golang"
+
+	// var price float32 = 50.5
+	// var price =50.5
+	price := 50.5
+
+	fmt.Println(price)
+}
